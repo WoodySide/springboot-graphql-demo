d293b0194c84c1230b1bb293d7d8155256300b79
Alex Vasyaev <alexwoodyside.com>
2023-02-01 20:06:40 +0300

    init

---
+++ src/main/java/com/woodyside/SpringbootGraphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.woodyside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraphqlDemoApplication.class, args);
+	}
+
+}
